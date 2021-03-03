997d5feb81b662806309229a3883c541343d0cda
ruheni <aleruheni@outlook.com>
2021-03-04 00:09:58 +0300

    minor update to app.ts and server.ts

--- api/app.ts
+++
@@ -1,5 +0,0 @@
-import { server } from './server'
-
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`)
-})
--- api/server.ts
+++ api/server.ts
@@ -2,7 +2,11 @@ import { ApolloServer } from 'apollo-server'
 import { context } from './context'
 import { schema } from './schema'
 
-export const server = new ApolloServer({
+const server = new ApolloServer({
   schema,
   context,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
 })
